dd97c8c4b92dce1fd8f902a5c9b78e3a46250b01
panda <p4nd4@posteo.de>
2023-07-22 17:35:45 +0200

    set vim as default editor

--- configuration.nix
+++ configuration.nix
@@ -167,7 +167,10 @@
     pkgs.virtualboxWithExtpack
     pkgs.sxiv
   ];
-
+  environmen.variables = {
+    "EDITOR" = "vim";
+  }
+;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
