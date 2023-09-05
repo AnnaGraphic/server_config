3f9125c81b43f33dc8501999b725a6d904d39cd8
panda <p4nd4@posteo.de>
2023-09-05 22:05:58 +0200

    textadventure: init at 0.0.0

--- kuno/pkgs/default.nix
+++ kuno/pkgs/default.nix
@@ -1,5 +1,6 @@
 self: super: {
   htgen = self.callPackage ./htgen.nix {};
   htgen-paste = self.callPackage ./htgen-paste.nix {};
+  textadventure = self.callPackage ./textadventure.nix {};
   tictactoe = self.callPackage ./tictactoe.nix {};
 }
