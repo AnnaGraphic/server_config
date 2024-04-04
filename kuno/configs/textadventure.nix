95874e5e8312d2e16a6229d889e41393606664a9
panda <p4nd4@posteo.de>
2024-04-04 18:52:28 +0200

    update config for new domain

--- kuno/configs/textadventure.nix
+++ kuno/configs/textadventure.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   services.nginx.enable = true;
-  services.nginx.virtualHosts."textadventure.panda.krebsco.de" = {
+  services.nginx.virtualHosts."wohnungssuche.die-partei-berlin.de" = {
     locations."/".root = "${pkgs.textadventure}/lib/client";
   };
 }
