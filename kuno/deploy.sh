4c1eddffe081b41027156b2b3847cd055a0c7a7f
panda <p4nd4@posteo.de>
2025-01-04 16:11:41 +0100

    kuno: improve deploy.sh: update exclusion rules and rsync
    - Replace `-va` with `-rlptD` for more precise rsync option control.
    - Add exclusion for `kleingeist/` in server sync.
    - Ensure consistent and optimized file synchronization across deployments.

--- kuno/deploy.sh
+++ kuno/deploy.sh
@@ -7,12 +7,12 @@ set -efu
 name=kuno
 host=130.61.237.100
 
-rsync -va /home/panda/Sync/shared-secrets/hosts root@"$host":/etc/panda/hosts
-rsync -va /home/panda/Sync/kuno-secrets/ root@"$host":/etc/panda/secrets/
-rsync -va --delete --exclude=.git --exclude=/configuration.nix "$HOME"/projekte/server/ root@"$host":/etc/nixos/
+rsync -v -rlptD /home/panda/Sync/shared-secrets/hosts root@"$host":/etc/panda/hosts
+rsync -v -rlptD /home/panda/Sync/kuno-secrets/ root@"$host":/etc/panda/secrets/
+rsync -v -rlptD --delete --exclude=.git --exclude=/configuration.nix --exclude=kleingeist/ "$HOME"/projekte/server/ root@"$host":/etc/nixos/
 
 # for /server/kuno/pkgs/portfolio
-rsync -va --delete --exclude=.git /home/panda/projekte/portfolio/ root@"$host":/etc/panda/portfolio/
+rsync -v -rlptD --delete --exclude=.git /home/panda/projekte/portfolio/ root@"$host":/etc/panda/portfolio/
 
 ssh root@"$host" ln -vsnf "$name"/configuration.nix /etc/nixos/configuration.nix
 
