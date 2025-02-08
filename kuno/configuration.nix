dcc59466d3a59b3e2178b3b4adad21f4220a131a
panda <p4nd4@posteo.de>
2025-02-08 14:08:45 +0100

    kuno: add reverse ssh tunnel/prima

--- kuno/configuration.nix
+++ kuno/configuration.nix
@@ -107,6 +107,14 @@
     ];
   };
 
+  users.users.prima = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      # for reverse ssh tunnel from prima (see prima@prima:tunnelfummel)
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINDzjDjfvD9dWthQItx6iWlezgxPVSJCylpMiQVymRU3"
+    ];
+  };
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCfP2xrEhEJn/MKTFbO6ATCqVr5jeqHqyRP9c4KKCA6eZDoZyovZQL6pM80b566rzpJGvLeRSlU10qYT+ftJBP/xhrRN4Jj5Xhp6phViSGz7uAb1uTWr93aO4r8XvNRzbjLreomsNQjyabrrEO+9K/fTQfwIfNxx52He3UTPg6Nq08FJOoqHarEFNRQqbgxr+Ad/wrXz8wZp7myUI5KrJppZ4t9VUElgfpPGHLt5jXPqdU0dfvBEvNCCX+QnBnNrIma2zwt/s4J+MizenpRWR37HU++qnIwh3hxOD3IgF/yX8nUZNbXeBBcGFR9VKDOPWYlCRz6LTDr4iai5+Qgc5mRhtZCfWzv0quzE9jPfhxjs9uHHGiHgbaLR8425d0x+XjZW1j6V1OZuwEUvEkgLxO9s6lZ1nklIjRPUNufeFTJ89VkVJd5iGIeQuBlyk2xXiNi0tMo8/JtNokPiwkYCNlMjvyRUm95DbZk+7mCqvNJHpMtPH46s3n0p0gzWYBI2q8= panda@spezi"
   ];
