18ba0bac904510689b75e740a2b8ece6de821c0a
panda <p4nd4@posteo.de>
2025-02-08 18:37:35 +0100

    komplizin: improve deploy.sh: update exclusion rules and rsync - Replace `-va` with `-rlptD` for more precise rsync option control. - Add exclusion for `kleingeist/` in server sync. - Ensure consistent and optimized file synchronization across deployments.

--- komplizin/deploy.sh
+++ komplizin/deploy.sh
@@ -7,9 +7,13 @@ set -efu
 name=komplizin
 host=komplizin.m
 
-rsync -va /home/panda/Sync/shared-secrets/hosts root@"$host":/etc/panda/hosts
-rsync -va /home/panda/Sync/komplizin-secrets/ root@"$host":/etc/panda/secrets/
-rsync -va --delete --exclude=.git --exclude=/configuration.nix "$HOME"/projekte/server/ root@"$host":/etc/nixos/
+rsync -rlptD /home/panda/Sync/shared-secrets/hosts root@"$host":/etc/panda/hosts
+rsync -rlptD /home/panda/Sync/komplizin-secrets/ root@"$host":/etc/panda/secrets/
+rsync -rlptD --delete \
+    --exclude=.git \
+    --exclude=/configuration.nix \
+    --exclude=/kleingeist \
+    "$HOME"/projekte/server/ root@"$host":/etc/nixos/
 
 ssh root@"$host" ln -vsnf "$name"/configuration.nix /etc/nixos/configuration.nix
 
