1b09d15712936c7ed19f03d0f159c388290f93a5
panda <p4nd4@posteo.de>
2025-07-08 19:41:10 +0200

    kuno: add cpu usage to monitoring

--- kuno/configs/uptime-kuma.nix
+++ kuno/configs/uptime-kuma.nix
@@ -41,4 +41,44 @@
       Persistent = true;
     };
   };
+  # check cpu usage
+    systemd.services.check-cpu-usage = {
+    wantedBy = [ "multi-user.target" ];
+    path = [
+      pkgs.coreutils
+      pkgs.curl
+      pkgs.gawk
+    ];
+    serviceConfig = {
+      ExecStart = pkgs.writers.writeDash "check-cpu-usage" ''
+      set -eu
+
+      TOKEN=$(cat /etc/panda/secrets/kuma-token-cpu-load)
+      PUSH_URL="https://kuno.panda.krebsco.de/api/push/$TOKEN"
+
+      # get average cpu load of the last 1 minute
+      LOAD=$(awk '{print $1}' /proc/loadavg)
+      CPU_CORES=$(nproc)
+
+      # get load in %
+      LOAD_PCT=$(echo "$LOAD $CPU_CORES" | awk '{ printf "%.0f", ($1 / $2) * 100 }')
+
+      echo check-cpu-usage $LOAD_PCT%
+
+      if [ "$LOAD_PCT" -lt 85 ]; then
+        # TODO log LOAD_PCT
+        curl -fsS "$PUSH_URL?status=up&msg=CPU%20$LOAD_PCT%&ping=CPU"
+      else
+        curl -fsS "$PUSH_URL?status=down&msg=HIGH%20CPU%20$LOAD_PCT%&ping=CPU"
+      fi
+      '';
+    };
+  };
+  systemd.timers.check-cpu-usage = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* *:*:00/2";
+      Persistent = true;
+    };
+  };
 }
