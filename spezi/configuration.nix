373d862d0847bf86c5f13bec9876b3b71bad9f22
panda <p4nd4@posteo.de>
2025-07-22 20:39:10 +0200

    spezi: init auto usb tethering

--- spezi/configuration.nix
+++ spezi/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ../configs/wireguard.nix
       ../configs/dic.nix
+      ./configs/auto-tether.nix
       ./hardware-configuration.nix
       ./shell.c-base.org.nix
       ./wetter.nix
