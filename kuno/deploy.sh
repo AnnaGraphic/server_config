40ee780173ce5ef1088f3cc5e26badda7eb36a95
panda <p4nd4@posteo.de>
2025-09-09 20:00:55 +0200

    deploy: ~/Sync/*-secrets -> ~/Sync/secrets/*

--- kuno/deploy.sh
+++ kuno/deploy.sh
@@ -7,9 +7,9 @@ set -efu
 name=kuno
 host=130.61.237.100
 
-rsync -v -rlptD /home/panda/Sync/shared-secrets/hosts root@"$host":/etc/panda/hosts
-rsync -v -rlptD /home/panda/Sync/kuno-secrets/ root@"$host":/etc/panda/secrets/
-rsync -v -rlptD --delete --exclude=.git --exclude=/configuration.nix --exclude=kleingeist/ "$HOME"/projekte/server/ root@"$host":/etc/nixos/
+rsync -v -rlptD /home/panda/Sync/secrets/shared/hosts root@"$host":/etc/panda/hosts
+rsync -v -rlptD /home/panda/Sync/secrets/kuno/ root@"$host":/etc/panda/secrets/
+rsync -v -rlptD --delete --exclude=.git --exclude=/configuration.nix --exclude=kleingeist/ "$HOME"/Sync/code/server/ root@"$host":/etc/nixos/
 
 # for /server/kuno/pkgs/portfolio
 rsync -v -rlptD --delete --exclude=.git /home/panda/projekte/portfolio/ root@"$host":/etc/panda/portfolio/
--- wsl2/deploy.sh
+++ wsl2/deploy.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 
 # usage: ./deploy.sh
+# TODO: also deploy /etc/panda/secrets/wireguard-private-key (currently it has to be done manually)
 
 set -efu
 
 name=wsl2
 host=wsl2.m
 
-rsync -rlptD /home/panda/Sync/shared-secrets/hosts root@"$host":/etc/panda/hosts
-rsync -rlptD /home/panda/Sync/wsl2-secrets/ root@"$host":/etc/panda/secrets/
+rsync -rlptD /home/panda/Sync/secrets/shared/hosts root@"$host":/etc/panda/hosts
+rsync -rlptD /home/panda/Sync/secrets/wsl root@"$host":/etc/panda/secrets/
 rsync -rlptD --delete \
     --exclude=.git \
     --exclude=/configuration.nix \
