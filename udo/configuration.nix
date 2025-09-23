31c5964eb3a5e3f69fa3891f2fea36274c981f59
panda <p4nd4@posteo.de>
2025-09-23 22:10:26 +0200

    udo: plasma5 -> plasma6 because nixos 25.05

--- udo/configuration.nix
+++ udo/configuration.nix
@@ -121,7 +121,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "panda";
   services.xserver.layout = "us";
