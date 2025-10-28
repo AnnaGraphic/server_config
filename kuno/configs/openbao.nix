a1a78a34dbb7d6a0114ccd4484e04f228976f0a0
panda <p4nd4@posteo.de>
2025-10-28 21:29:32 +0100

    kuno: services.{vault => openbao}

--- kuno/configs/openbao.nix
+++ kuno/configs/openbao.nix
@@ -22,42 +22,46 @@
   nixpkgs.config.allowUnfreePredicate = pkg: lib.getName pkg == "vault";
 
 #  systemd.services.vault.serviceConfig.ExecStart = lib.mkForce "${pkgs.openbao}/bin/openbao server ${configOptions}";
-  services.vault = {
+  services.openbao = {
     enable = true;
-    package = pkgs.writers.writeDashBin "vault" ''
-      exec ${pkgs.openbao}/bin/bao "$@"
-    '';
+#    package = pkgs.writers.writeDashBin "vault" ''
+#      exec ${pkgs.openbao}/bin/bao "$@"
+#    '';
 
-    # address = for extern Clients Vault-CLI, API
-    address = "[::]:8200";
-    extraConfig = ''
-      api_addr = "https://kuno.m:8200"
-      cluster_addr = "https://kuno.m:8201"
-    '';
+    settings = {
+      api_addr = "https://kuno.m:8200";
+      cluster_addr = "https://kuno.m:8201";
 
-    storageBackend = "raft";
-    storageConfig = ''
-      path = "/var/lib/openbao"
-      node_id = "raft_node_bao_kuno"
+      storage = {
+        raft = {
+          path = "/var/lib/openbao";
+          node_id = "raft_node_bao_kuno";
+          retry_join = [
+            {
+              leader_api_addr = "https://spezi.m:8200";
+              leader_client_cert_file = "/etc/nixos/kuno/certs/kuno-openbao-tls.crt";
+              leader_client_key_file = "/run/credentials/openbao.service/tls.key";
+            }
+            {
+              leader_api_addr = "https://wsl2.m:8201";
+              leader_client_cert_file = "/etc/nixos/kuno/certs/kuno-openbao-tls.crt";
+              leader_client_key_file = "/run/credentials/openbao.service/tls.key";
+            }
+          ];
+        };
+      };
 
-      retry_join {
-        leader_api_addr = "https://spezi.m:8200"
-      }
-
-      retry_join {
-        leader_api_addr = "https://wsl2.m:8201"
-      }
-    '';
-
-    tlsCertFile = "/etc/nixos/kuno/certs/kuno-openbao-tls.crt"; # TODO SubjectAltName muss die jeweilige api_addr enthalten.
-    tlsKeyFile  = "/run/credentials/vault.service/tls.key";
+      listener.default = {
+          type = "tcp";
+          address = "[::]:8200";
+          tls_cert_file = "/etc/nixos/kuno/certs/kuno-openbao-tls.crt";
+          tls_key_file  = "/run/credentials/openbao.service/tls.key";
+      };
+    };
   };
-
-# |TODO eigene Service config
-  systemd.services.vault.serviceConfig.LoadCredential = [
+# TODO eigene Service config
+  systemd.services.openbao.serviceConfig.LoadCredential = [
     "tls.key:/etc/panda/secrets/kuno-openbao-tls.key"
   ];
-
-  # uncomment this to automatically restart vault.service after deployment
-  #systemd.services.vault.serviceConfig.Restart = lib.mkForce "always";
 }
+          #tls_key_file = "/etc/panda/secrets/kuno-openbao-tls.key";
