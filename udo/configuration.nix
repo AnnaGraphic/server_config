5c440ad03084dce274f53a9b23b5e90ee542a7b8
panda <p4nd4@posteo.de>
2026-02-10 20:46:25 +0100

    udo: add wireguard
    this is part of 654f90d

--- udo/configuration.nix
+++ udo/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       #../configs/wireguard.nix
       ../configs/dic.nix
+      ../configs/wireguard.nix
       ./hardware-configuration.nix
       ./shell.c-base.org.nix
       ./wetter.nix
