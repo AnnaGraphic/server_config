75806b2cb21a587797282f7d3a0c10aae0dee326
panda <p4nd4@posteo.de>
2026-02-10 22:33:43 +0100

    openbao: add softhsm2

--- configs/openbao.nix
+++ configs/openbao.nix
@@ -8,6 +8,9 @@
 # - adjust fw settings
 # - tsl certs
 # - check: path = "/var/lib/openbao" writable for service user
+# - generate hsm stuff:
+#   openssl genpkey -algorithm RSA -out openbao.pem -pkeyopt rsa_keygen_bits:4096
+#   # directrory: etc/secrets/<host>
 { config, lib, pkgs, ... }:
 
 let
@@ -49,6 +52,8 @@ in {
   environment.systemPackages = [
     # bao cli
     pkgs.openbao
+    pkgs.opensc
+    pkgs.softhsm
   ];
 
   networking.firewall.allowedTCPPorts = [
@@ -62,6 +67,15 @@ in {
     settings = {
       api_addr = hosts.${host}.apiAddr;
       cluster_addr = hosts.${host}.clusterAddr;
+
+      seal.pkcs11 = {
+        lib = "${pkgs.softhsm}/lib/softhsm/libsofthsm2.so";
+        token_label = "OpenBao";
+        pin = "6666";
+        key_label = "bao-root-key-rsa";
+        key_id = "0xd00f";
+        rsa_oaep_hash = "sha1";
+      };
 
       storage.raft = {
         path = "/var/lib/openbao";
@@ -77,8 +91,59 @@ in {
       };
     };
   };
+
 # TODO eigene Service config
   systemd.services.openbao.serviceConfig.LoadCredential = [
     hosts.${host}.keyCred
+    "openbao.pem:${baseSecretDir}/openbao.pem"
   ];
+
+  systemd.services.openbao.preStart = ''
+    set -efu
+
+    token_directory=/var/lib/openbao/softhsm2/tokens
+    root_key_file=/run/credentials/openbao.service/openbao.pem
+    so_pin=77776666
+    pin=6666
+    key_id=A1BB
+
+    ${pkgs.coreutils}/bin/rm -rf "$token_directory"
+    ${pkgs.coreutils}/bin/mkdir -m 0700 -p "$token_directory"
+
+    tmpfile=/tmp/softhsm2.p8
+
+    ${pkgs.openssl}/bin/openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt \
+      -in "$root_key_file" -out "$tmpfile"
+
+    ${pkgs.softhsm}/bin/softhsm2-util --init-token --slot 0 --label OpenBao \
+      --so-pin "$so_pin" --pin "$pin"
+    ${pkgs.softhsm}/bin/softhsm2-util --import "$tmpfile" --token OpenBao \
+      --label bao-root-key-rsa --pin "$pin" --id "$key_id"
+
+    ${pkgs.opensc}/bin/pkcs11-tool -v \
+      --module ${pkgs.softhsm}/lib/softhsm/libsofthsm2.so -O -l -p "$pin"
+  '';
+
+  systemd.services.openbao.environment = {
+    # TODO tokendir from variable
+    SOFTHSM2_CONF = pkgs.writeText "softhsm2.conf" ''
+      directories.tokendir = /var/lib/openbao/softhsm2/tokens
+      log.level = INFO
+      objectstore.backend = file
+      slots.mechanisms = ALL
+      slots.removable = false
+    '';
+  };
 }
+#{
+#  systemd.services.openbao-configurator = {
+#    partOf = [ "openbao.service" ];
+#    script = ''
+#      until kann-ich-openbao-api?; do sleep 1; done
+#      mach-api-kram
+#    '';
+#    ServiceConfig = {
+#      Type = "oneshot";
+#    };
+#  };
+#}
